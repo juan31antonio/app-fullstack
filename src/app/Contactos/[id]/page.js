1b73d43aab855c00cdf3e4a465e5fa4b6d731080
juan31antonio <juanantoniocorder32@gmail.com>
2025-01-13 23:37:54 +0100

    Arreglado un unico usuario

--- src/app/Contactos/[id]/page.js
+++ src/app/Contactos/[id]/page.js
@@ -5,33 +5,33 @@ import { useEffect, useState, use } from "react";
 export default function User({params}) {
 
     const { id } = use(params);
-    const [user, setUser] = useState()
+    const [usuario, setUsuario] = useState(null)
 
 
   async function fetchUser(){
     const url = "/api/contactos/contacto?id=" + id
     const response = await fetch(url);
     const user = await response.json()
-    setUser(user)
+    setUsuario(user[0])
   }
 
   
   useEffect( () => {fetchUser()}, [])
 
 
-  if (!user) {
+  if (!usuario) {
     return <h1>Ese usuario no se ha encontrado</h1>;
   }
 
     return (
         <div>
             <h3>Usuario: </h3>
-            <p><strong>ID:</strong> {user.id}</p>
-            <p><strong>Nombre:</strong> {user.nombre}</p>
-            <p><strong>Apellidos:</strong> {user.apellidos}</p>
-            <p><strong>Correo:</strong> {user.correo }</p>
-            <p><strong>Numero de Telefono:</strong> {user.numero_telefono}</p>
-            <p><strong>Fecha de Nacimiento:</strong> {user.fecha_nacimiento}</p>
+            <p><strong>ID:</strong> {usuario.id}</p>
+            <p><strong>Nombre:</strong> {usuario.nombre}</p>
+            <p><strong>Apellidos:</strong> {usuario.apellidos}</p>
+            <p><strong>Correo:</strong> {usuario.correo }</p>
+            <p><strong>Numero de Telefono:</strong> {usuario.numero_telefono}</p>
+            <p><strong>Fecha de Nacimiento:</strong> {usuario.fecha_nacimiento}</p>
         </div>
     )
 }
